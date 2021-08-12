062ccbc2f253c5b15d0a33936ba49386dd1f4713
Johny <johnyj2@nih.gov>
2021-08-12 16:33:32 -0400

    Fix: Updated host name OM-4121

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -9,7 +9,7 @@ sed -i.bak \
 rm deployment.yaml.bak
 
 sed -i.bak \
-    -e "s/EACP_HOSTNAME_VALUE/${EACP_HOSTNAME}/g" \
+    -e "s/EA_HOSTNAME_VALUE/${EA_HOSTNAME}/g" \
     -e "s/ARS_ALB_TAG_VALUE/${ARS_ALB_TAG}/g" \
     -e "s/ARS_ALB_SG_VALUE/${ARS_ALB_SG}/g" \
     -e "s/ENVIRONMENT_TAG_VALUE/${ENVIRONMENT_TAG}/g" \
