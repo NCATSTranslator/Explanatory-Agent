7d27b44b3127837cae8e3760fe6b2a4723cd57ae
Johny <johnyj2@nih.gov>
2021-09-29 18:17:35 -0400

    Fix: Updated secret

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -5,6 +5,8 @@ export $(egrep -v '^#' .env)
 sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
     -e "s/ARS_ALLOWED_HOSTS_VALUE/${ARS_ALLOWED_HOSTS}/g" \
+    -e "s|DB_USER_VALUE|${DB_USER}|g" \
+	-e "s|DB_PASSWORD_VALUE|${DB_PASSWORD}|g" \
     deployment.yaml
 rm deployment.yaml.bak
 
@@ -17,6 +19,7 @@ sed -i.bak \
 rm ingress.yaml.bak
 
 kubectl apply -f namespace.yaml
+kubectl apply -f secret.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f services.yaml
 kubectl apply -f ingress.yaml
