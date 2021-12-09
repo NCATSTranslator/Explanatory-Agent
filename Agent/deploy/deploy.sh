a140793686efcfccf52777fb0ad6597a27d0df61
Jijo070 <jijo.johjny07@gmail.com>
2021-12-09 15:58:58 -0500

    Fix: Updayted variables

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -10,15 +10,15 @@ rm secret.yaml.bak
 
 sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
-    -e "s/ARS_ALLOWED_HOSTS_VALUE/${ARS_ALLOWED_HOSTS}/g" \
+    -e "s/EA_ALLOWED_HOSTS_VALUE/${EA_ALLOWED_HOSTS}/g" \
     -e "s|DB_HOST_VALUE|${DB_HOST}|g" \
     deployment.yaml
 rm deployment.yaml.bak
 
 sed -i.bak \
     -e "s/EA_HOSTNAME_VALUE/${EA_HOSTNAME}/g" \
-    -e "s/ARS_ALB_TAG_VALUE/${ARS_ALB_TAG}/g" \
-    -e "s/ARS_ALB_SG_VALUE/${ARS_ALB_SG}/g" \
+    -e "s/EA_ALB_TAG_VALUE/${EA_ALB_TAG}/g" \
+    -e "s/EA_ALB_SG_VALUE/${EA_ALB_SG}/g" \
     -e "s/ENVIRONMENT_TAG_VALUE/${ENVIRONMENT_TAG}/g" \
     ingress.yaml
 rm ingress.yaml.bak
