54194ca1d781495ee8fdb5e0a27325fd67d531b1
bettyli037 <jingyi.li@axleinfo.com>
2022-05-02 14:40:51 -0400

    feat: update Jenkisfile and deploy.sh

--- Agent/deploy/ea/deploy.sh
+++ Agent/deploy/ea/deploy.sh
@@ -9,14 +9,14 @@ sed -i.bak \
     -e "s|DB_USERNAME_VALUE|${DB_USERNAME}|g" \
     -e "s|DB_PASSWORD_VALUE|${DB_PASSWORD}|g" \
     -e "s|DB_SCHEMA_VALUE|${DB_SCHEMA}|g" \
-    secret.yaml
-rm secret.yaml.bak
+    templates/secret.yaml
+rm templates/secret.yaml.bak
 
 sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
     -e "s/EA_ALLOWED_HOSTS_VALUE/${EA_ALLOWED_HOSTS}/g" \
     -e "s|DB_HOST_VALUE|${DB_HOST}|g" \
-    deployment.yaml
-rm deployment.yaml.bak
+    templates/deployment.yaml
+rm templates/deployment.yaml.bak
 
 helm -n ${namespace} upgrade --install ${projectName} -f values-ci.yaml ./
