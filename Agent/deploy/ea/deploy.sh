aa2e81e9c46ebb095166ed74f3e2a7b2c26566dd
bettyli037 <jingyi.li@axleinfo.com>
2022-05-03 10:49:30 -0400

    feat: update helm chart

--- Agent/deploy/ea/deploy.sh
+++ Agent/deploy/ea/deploy.sh
@@ -6,15 +6,8 @@ namespace="chp"
 export $(egrep -v '^#' .env)
 
 sed -i.bak \
-    -e "s|DB_USERNAME_VALUE|${DB_USERNAME}|g" \
-    -e "s|DB_PASSWORD_VALUE|${DB_PASSWORD}|g" \
-    -e "s|DB_SCHEMA_VALUE|${DB_SCHEMA}|g" \
-    templates/secret.yaml
-rm templates/secret.yaml.bak
-
-sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
-    values-ci.yaml
-rm values-ci.yaml.bak
+    values.yaml
+rm values.yaml.bak
 
-helm -n ${namespace} upgrade --install ${projectName} -f values-ci.yaml ./
+helm -n ${namespace} upgrade --install ${projectName} -f values.yaml -f values-ci.yaml ./
