87b5100f701a7aac370aaf8fa4dd36ef02bbd726
bettyli037 <jingyi.li@axleinfo.com>
2022-12-28 14:48:28 -0500

    feat: update Jenkinsfile and deploy.sh

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -12,5 +12,5 @@ sed -i.bak \
     -e "s/USERNAME_VALUE/${username}/g;s/PASSWORD_VALUE/${password}/g" \
     values-ncats.yaml
 rm values-ncats.yaml.bak
-
+sudo su -
 helm -n ${namespace} upgrade --install ${projectName} -f values-ncats.yaml ./
