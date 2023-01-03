5a06d9aa7ee12c1527f6c68712b341be2ed201f8
bettyli037 <jingyi.li@axleinfo.com>
2023-01-03 13:42:53 -0500

    feat: update Jenkinsfile and deploy.sh

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -3,17 +3,14 @@
 projectName="agent"
 namespace="chp"
 
-username=`aws --region ${AWS_REGION} secretsmanager get-secret-value --secret-id TranslatorCIExplanatoryAgentDBSecret | jq --raw-output .SecretString | jq -r ."username"`
-password=`aws --region ${AWS_REGION} secretsmanager get-secret-value --secret-id TranslatorCIExplanatoryAgentDBSecret | jq --raw-output .SecretString | jq -r ."password"`
-
 sed -i.bak \
     -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
     values.yaml
 rm values.yaml.bak
 
 sed -i.bak \
-    -e "s/USERNAME_VALUE/${username}/g;s/PASSWORD_VALUE/${password}/g" \
+    -e "s/DB_USERNAME_VALUE/${DB_USERNAME}/g;s/DB_PASSWORD_VALUE/${DB_USERNAME}/g" \
+    -e "s/APP_DB_USERNAME_VALUE/${APP_DB_USERNAME}/g;s/APP_DB_PASSWORD_VALUE/${APP_DB_PASSWORD}/g"
     values-ncats.yaml
 rm values-ncats.yaml.bak
-sudo su -
 helm -n ${namespace} upgrade --install ${projectName} -f values-ncats.yaml ./
