2b4655360fcf0656c8909563d935ad4f8a0e3de2
bettyli037 <jingyi.li@axleinfo.com>
2023-01-03 13:53:16 -0500

    feat: update deploy.sh

--- Agent/deploy/deploy.sh
+++ Agent/deploy/deploy.sh
@@ -10,7 +10,7 @@ rm values.yaml.bak
 
 sed -i.bak \
     -e "s/DB_USERNAME_VALUE/${DB_USERNAME}/g;s/DB_PASSWORD_VALUE/${DB_USERNAME}/g" \
-    -e "s/APP_DB_USERNAME_VALUE/${APP_DB_USERNAME}/g;s/APP_DB_PASSWORD_VALUE/${APP_DB_PASSWORD}/g"
+    -e "s/APP_DB_USERNAME_VALUE/${APP_DB_USERNAME}/g;s/APP_DB_PASSWORD_VALUE/${APP_DB_PASSWORD}/g" \
     values-ncats.yaml
 rm values-ncats.yaml.bak
 helm -n ${namespace} upgrade --install ${projectName} -f values-ncats.yaml ./
